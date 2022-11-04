508b6e7fdb3e464cc8c3ce06f67f9cad5c75e698
Venom-61 <subhendukarmakar61@gmail.com>
2022-11-04 17:43:58 +0530

    a.cpp

---
+++ a.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+// 
+
+void solve() {
+    
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+
+    int tc = 1;
+    cin >> tc;
+    for(int i = 1; i <= tc; i++) {
+        // cout << "Case: #" << i << " ";
+        solve();
+    }
+
+    return 0;
+}
