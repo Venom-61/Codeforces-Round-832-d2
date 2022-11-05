66fa61c62ae3f9df895f05b61a20ccf3d210095c
Venom-61 <subhendukarmakar61@gmail.com>
2022-11-05 17:04:01 +0530

    Two Groups

--- a.cpp
+++ a.cpp
@@ -3,10 +3,19 @@ using namespace std;
 
 #define ll long long
 
-// 
+// A. Two Groups 
 
 void solve() {
-    
+    int n;
+    cin >> n;
+    long long sum = 0;
+    for(int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        sum += x;
+    }
+
+    cout << abs(sum) << "\n";
 }
 
 int32_t main() {
